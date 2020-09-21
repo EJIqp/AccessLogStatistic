9feb6859c493871d878b592bedfc182cb70edf7b
Nikita Kulichkov <ejiqp91@gmail.com>
2020-09-22 01:06:37 +0300

    баг в arrayService

--- app/helpers/ArrayService.php
+++ app/helpers/ArrayService.php
@@ -7,11 +7,11 @@ class ArrayService
     /**
      * Проверка наличия строки в массиве
      * @param  string       $needle   искомое значение
-     * @param  string       $haystack массив в котором ищем
+     * @param  array        $haystack массив в котором ищем
      * @param  bool|boolean $strict   соответствие типов
      * @return bool                   true - если найден, false - если нет
      */
-    public static function inArray(string $needle , string $haystack, bool $strict = FALSE): bool{   
+    public static function inArray(string $needle , array $haystack, bool $strict = FALSE): bool{   
         $result = in_array($needle,$haystack,$strict);
         
         return (bool)$result;
